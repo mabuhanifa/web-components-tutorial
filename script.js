824ecc0d2d115434f163d4f3662f6d2d33f485ac
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-25 15:38:24 +0600

    <style>

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-class MyButton extends HTMLElement{
+class MyButton extends HTMLElement {
+  constructor() {
+    super();
+  }
 
+  connectedCallback() {
+    const button = `
+    
+    <button>${this.innerHTML}</button>
+    <style>
+      button {
+        outline: none;
+        border: none;
+        padding: 20px;
+        background: #f80000;
+        border-radius: 0.25rem;
+      }
+    </style>
+    `;
+    this.innerHTML = button;
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(name, oldValue, newValue);
+  }
+
+  disconnectedCallback() {}
 }
+
+customElements.define("my-button", MyButton);
